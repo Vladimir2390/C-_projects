7320ffa52b1e20c6f04616ab1228d80d2906a04d
Vladimir <air90@bk.ru>
2022-08-11 19:44:10 +0500

    1

--- Task0708/Program.cs
+++ Task0708/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿/*
+**Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int number = new Random().Next(100,1000);
+string inputNumber = Console.ReadLine();
+Console.WriteLine($"{inputNumber[0]}{inputNumber[2]}");
