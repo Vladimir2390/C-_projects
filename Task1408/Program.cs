fed15eceff3f95da25772f3dacf0fc1025298fbe
Vladimir <air90@bk.ru>
2022-08-15 23:06:47 +0500

    4ДЗ 2з

---
+++ Task1408/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+/*
+System.Console.WriteLine("Введите число A : ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B : ");
+int B = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Math.Pow(A,B ));
+*/
+/*
+Задача 27: Напишите программу, которая принимает 
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+/*
+string inputNumber = Console.ReadLine();
+Console.WriteLineConsole.WriteLine($"{inputNumber[0]}{inputNumber[2]}");
+*/
+
+/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+/*
+int[] nums = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    nums[i] = new Random().Next();
+    Console.Write(nums[i] + ", ");
+}
+*/
